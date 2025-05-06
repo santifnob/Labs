8184308603d4b8ff154fa15a55239976c1559aa4
Santiago Ferrero <santifnob@gmail.com>
2025-05-06 19:20:56 -0300

    Ej09 - U2 Sintaxis

---
+++ Unidad.2.Capitulo.1.Lab.1.Sintaxis/Ej9/Ej9/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Ej9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresar un numero de filas: ");
+
+            int filas = int.Parse(Console.ReadLine());
+            int columnas = (filas * 2) - 1;
+            int puntoMedio = columnas /2;
+
+            int desplazamiento = 0;
+
+            for (int i = 0; i < filas; i++)
+            {
+                string cadena = string.Empty;
+                
+                for (int j = 0; j < columnas; j++)
+                {
+                    if (j >= (puntoMedio - desplazamiento) && j<= (puntoMedio + desplazamiento)) cadena = cadena + "*";
+                    else cadena = cadena + " ";
+                }
+
+                Console.WriteLine(cadena);
+
+                desplazamiento++;
+            }
+        }
+    }
+}
