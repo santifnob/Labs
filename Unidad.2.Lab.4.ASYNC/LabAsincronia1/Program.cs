a7b81245f1ecfbd4b5583fdc13827b654617c724
Santiago Ferrero <santifnob@gmail.com>
2025-05-19 17:50:49 -0300

    U2-L4

---
+++ Unidad.2.Lab.4.ASYNC/LabAsincronia1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+namespace LabAsincronia1
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            CompararSincronoVsAsincrono();
+        }
+
+        public static void SimularOperacionPesada() {
+            Thread.Sleep(3000);
+        }
+
+        public static async Task SimularOperacionPesadaAsyng(Stopwatch sw)
+        {
+            await Task.Delay(3000); Console.WriteLine("Termino el delay. Tiempo de ejecucion: " + sw.ElapsedMilliseconds + " ms"); 
+            //Le agregue el output para darme cuenta que en el fondo se esta ejecutando y se muestre si el input lo hice despues del lapso del delay
+        }
+
+        public static void CompararSincronoVsAsincrono()
+        {
+            Console.WriteLine("Inicio");
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            SimularOperacionPesada();
+            Console.WriteLine("Fin Sincrono. Demora de: " + stopwatch.ElapsedMilliseconds + "ms");
+            SimularOperacionPesadaAsyng(stopwatch);
+            Console.WriteLine("Ingresar texto: "); Console.ReadLine();
+            stopwatch.Stop();
+            Console.WriteLine("Fin Total. Demora de: " + stopwatch.ElapsedMilliseconds + "ms");
+}
+
+
+    }
+}
